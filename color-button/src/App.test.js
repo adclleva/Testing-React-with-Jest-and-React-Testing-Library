7e09cea038ba85f73386ff8cb785b2547f778318
adclleva <arvin.lleva@yahoo.com>
2022-08-14 17:08:54 -0700

    added testing notes

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn testing/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/**
+ * assertions use an expect method
+ * it expects an argument, matcher, and matcher argumet
+ */
+
+/**
+ * jest-dom, comes with the create-react-app
+ * src/setup Tests.sj imports it before each test and makes matchers available
+ * DOM-based matchers
+ *  ex: toBeVisible() or toBeChecked()
+ */
