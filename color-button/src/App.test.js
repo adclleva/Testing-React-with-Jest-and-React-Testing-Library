840ce0cd6a8243e4cd4ddb0b24c51116d1f74acc
Arvin Lleva <arvin.lleva@yahoo.com>
2022-08-25 22:06:27 -0700

    tests that find button by toel and clicks button

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen , fireEvent} from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,15 +9,18 @@ test('button has correct initial color', () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
+  // click button
+  fireEvent.click(colorButton)
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'})
+
+  // expect the button text to be 'Change to Red'
+  expect(colorButton.textContent).toBe('Change to red')
 });
 
-test('button has correct initial test', () => {
 
-});
-
-test('button turns blue when clicked', () => {
-
-});
 
 
 /**
