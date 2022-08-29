07b6f9ce607b8328381bda7d5095927d9889dd4f
Arvin Lleva <arvin.lleva@yahoo.com>
2022-08-29 09:51:57 -0700

    test with mock server worker

---
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i }); // the $ means the end of the expression
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]); // for any immutable type, you use toEqual
+});
