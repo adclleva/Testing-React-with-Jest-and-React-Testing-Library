0fd750f03e574d91e8ce2ab207031b6d7e57f495
Arvin Lleva <arvin.lleva@yahoo.com>
2022-09-05 22:46:47 -0700

    created the AlertBanner and OrderEntry and added error ot OPtions

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -14,8 +16,13 @@ export default function Options({ optionType }) {
       .then((response) => setItems(response.data))
       .catch((error) => {
         // TODO:handle error response
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: replace `null` with TopppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
